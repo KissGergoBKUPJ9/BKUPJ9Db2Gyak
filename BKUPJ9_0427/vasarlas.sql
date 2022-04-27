98c6a33c3fac1198e9bcb650b2eb8bf9675d636d
KissGergoBKUPJ9 <76010270+KissGergoBKUPJ9@users.noreply.github.com>
2022-04-27 10:58:01 +0200

    Add files via upload

---
+++ BKUPJ9_0427/vasarlas.sql
@@ -0,0 +1,7 @@
+Create table Vasarlas(
+    Sorszam Number(38,0) Not NUll,
+    Idopont Timestamp(6),
+    TKOD Char(3) Not Null,
+    Darab Number(38,0),
+    VID Char(3)
+);
---
+++ BKUPJ9_0427/vasarlas_feltolt.sql
@@ -0,0 +1,8 @@
+Begin
+    insert into Vasarlas values(1, sysdate, 't01',1, 'v06');
+    insert into Vasarlas values(2, sysdate, 't02',1, 'v07');
+    insert into Vasarlas values(3, sysdate, 't03',2, 'v08');
+    insert into Vasarlas values(4, sysdate, 't04',2, 'v09');
+    insert into Vasarlas values(5, sysdate, 't05',3, 'v02');
+    insert into Vasarlas values(6, sysdate, 't06',4, 'v03');
+End;
